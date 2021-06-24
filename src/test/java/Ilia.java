c7ead58833daea1070bed66c3c15010b9c7bd807
Ilia Ponomarev <ponomid@ya.ru>
2021-06-23 18:41:53 -0700

    Ponomid test branch (#1)
    * refactoring
    
    * refactoring PR

--- src/test/java/Ilia.java
+++ src/test/java/Ilia.java
@@ -8,6 +8,7 @@ public class Ilia extends Base {
 
     @Test
     public void test() {
+        //checkings
 
         final String expectedLogoText = "Earnon.social";
         driver.get("https://earnon.social");
