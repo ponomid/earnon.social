f44a0a903cecee26b14758ef894fb116ad4c5c3f
Ilia Ponomarev <ponomid@ya.ru>
2021-06-25 16:36:32 -0700

    Refactoring

--- src/test/java/IliaTest.java
+++ src/test/java/IliaTest.java
@@ -14,7 +14,7 @@ public class IliaTest extends Base {
     public final String password = "Smmtool88";
 
     @Test
-    public void firstTest() {
+    public void testFirst() {
 
         final String expectedLogoText = "Earnon.social";
         getDriver().get("https://earnon.social");
@@ -23,43 +23,12 @@ public class IliaTest extends Base {
     }
 
     @Test
-    public void signInTest(){
-        //not working
+    public void testSignIn() {
+
         SignInPage signInPage = new MainPage(getDriver())
                 .clickSignIn()
                 .fillEmail(login)
                 .fillPassword(password);
-    }
-
-    @Test
-    public void signInTestTwo() {
-        //not working
-        getDriver().get("https://earnon.social");
-        getDriver().findElement(By.linkText("Sign In")).click();
-        getDriver().findElement(By.xpath("//input[@id='username']")).sendKeys(login);
-        getDriver().findElement(By.id("password")).sendKeys(password);
-        getDriver().findElement(By.xpath("//button[@name='action']"));
-    }
-
-    @Test
-    public void signUpTest() {
-        //not working
-        MainPage mainPage = new MainPage(getDriver());
-        mainPage.clickSignUp();
-
-        SignUpPage signUpPage = new SignUpPage(getDriver());
-        signUpPage.fillEmail();
-        signUpPage.fillPassword();
-    }
-
-    @Test
-    public void forgotPassTest() {
-        //not working
-        MainPage mainPage = new MainPage(getDriver());
-        mainPage.clickSignIn();
-
-        SignInPage signInPage = new SignInPage(getDriver());
-        signInPage.clickForgotPassword();
     }
 }
 
